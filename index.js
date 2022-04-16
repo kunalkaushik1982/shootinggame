3b835a297690771d37217b37d0a64c910468e527
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-04-16 14:13:14 +0530

    Create Player Class

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const canvas = document.createElement("canvas");
 document.querySelector(".myGame").appendChild(canvas);
 
-canvas.width=innerWidth
-canvas.height=innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const context = canvas.getContext("2d");
 
-context.beginPath();
-context.arc(100, 100, 10, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
-context.stroke()
+playerPosition = { x: canvas.width / 2, y: canvas.height / 2 };
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    context.beginPath();
+    context.arc(
+      this.x,
+      this.y,
+      this.radius,
+      (Math.PI / 180) * 0,
+      (Math.PI / 180) * 360,
+      false
+    );
+    context.fillStyle = this.color;
 
+    context.fill();
+  }
+//   update() {
+//     this.x += Math.random() * 10;
+//     this.y += Math.random() * 10;
+//   }
+}
+
+const pla = new Player(
+  playerPosition.x,
+  playerPosition.y,
+  15,
+  ` rgb(${Math.random() * 250},${Math.random() * 250},${Math.random() * 250})`
+);
+
+function animation() {
+  requestAnimationFrame(animation);
+  pla.draw();
+  //pla.update();
+}
+animation();
